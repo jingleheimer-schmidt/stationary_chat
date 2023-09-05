66fd4df356c606796a63db8a9fce259ffae9bc88
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2023-09-05 00:42:11 -0700

    Create settings.lua

---
+++ settings.lua
@@ -0,0 +1,12 @@
+
+local stationary_chat = {
+    type = "bool-setting",
+    name = "stationary_chat",
+    setting_type = "startup",
+    default_value = true,
+    order = "stationary_chat"
+}
+
+data:extend{
+    stationary_chat
+}
